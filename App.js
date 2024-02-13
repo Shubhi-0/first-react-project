2844332b422e7e5ebdfa42a253e67a71c800d0d6
Shubhi-0 <shubhiupadhyay31@gmail.com>
2024-02-14 02:09:29 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "hey hey"),
+    React.createElement("h2", {}, "h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "hey hey"),
+    React.createElement("h2", {}, "h2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
